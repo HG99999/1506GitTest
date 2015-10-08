4350e9845315cfab3096ea9504b1fd8e65192484
浅忆 <huguang927@huguang.local>
2015-10-08 11:58:42 +0800

    fewrwqr

---
+++ 2421421/2421421/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2421421
+//
+//  Created by 浅忆 on 15/10/8.
+//  Copyright (c) 2015年 浅忆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 2421421/2421421/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2421421
+//
+//  Created by 浅忆 on 15/10/8.
+//  Copyright (c) 2015年 浅忆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
